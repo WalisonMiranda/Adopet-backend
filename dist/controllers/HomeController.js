8b9fdb7ea62fce9a5d8bbfdf7f2748c8cfe41fbe
Walison Miranda <46504580+WalisonMiranda@users.noreply.github.com>
2022-05-05 11:14:01 -0300

    Delete dist directory

--- dist/controllers/HomeController.js
+++
@@ -1,8 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});class HomeController {
-  async index(req, res) {
-    res.json('Home');
-  }
-}
-
-// exporta já instanciado
-exports. default = new HomeController();
