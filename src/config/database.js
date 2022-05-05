31e4dcb4c6b7cb2f4835620522252434f2095289
Walison Miranda <walisonshido@gmail.com>
2022-05-05 01:23:31 -0300

    .

--- src/config/database.js
+++ src/config/database.js
@@ -7,6 +7,7 @@ module.exports = {
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE,
+  url: process.env.DATABASE_URL,
 
   define: {
     timestamps: true,
