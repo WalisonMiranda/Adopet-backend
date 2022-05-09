3a8758a09b080bc869ec77bc46fc18c980f5241a
Walison Miranda <walisonshido@gmail.com>
2022-05-09 12:18:39 -0300

    refactor: database conection

--- src/database/index.js
+++ src/database/index.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
+
 import Sequelize from 'sequelize';
-import databaseConfig from '../config/database';
 import Pet from '../models/Pet';
 import User from '../models/User';
 import Foto from '../models/Foto';
 
 const models = [User, Pet, Foto];
-const connection = new Sequelize(databaseConfig);
+const connection = new Sequelize(process.env.URL_CONECTION);
 
 models.forEach((model) => model.init(connection));
 models.forEach((model) => model.associate && model.associate(connection.models));
